e153ee9b44ef03e76e506a5f978c1167e62631df
Aditya Raj <adityaraj34@icloud.com>
2026-03-04 00:22:59 +0530

    frontend structure done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom' 
+import Home from './components/Home'
+import { Router } from 'express'
+import SignUp from './components/SignUp'
+import Login from './components/Login'
 
 const App = () => {
+  const [token,setToken]=useState(localStorage.getItem("token") || "")
+
+
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/register" element={<SignUp/>}/>
+        <Route path="/login" element={<Login setToken={setToken} />}/>
+        <Route path="home" element={<Home token={token} />}/>
+        
+        
+      </Routes>
+    </Router>
+    
   )
 }
 
